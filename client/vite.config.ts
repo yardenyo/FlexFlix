480486d78107d5505f8e096b67053e70623a9f2d
yarden <yarden@tigloo.io>
2023-04-02 21:41:01 +0300

    added client side.

---
+++ client/vite.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	server: {
+		port: 3000,
+	},
+	test: {
+		environment: "happy-dom",
+		globals: true,
+	},
+	plugins: [vue(), vueJsx()],
+	resolve: {
+		extensions: [".js", ".json", ".vue", ".scss", ".css"],
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+			img: path.resolve(__dirname, "./public/img"),
+		},
+	},
+	build: {
+		sourcemap: false,
+		minify: true,
+		assetsDir: "chunks",
+	},
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: `@use  "sass:math"; @import "./src/assets/scss/v2/legacy.scss"; @import "./src/assets/scss/common.scss";`,
+			},
+		},
+	},
+});
