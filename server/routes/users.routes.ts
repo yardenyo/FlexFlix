914e7eda1d05f2a4915803c9eb8c989e93afa52e
yardenyo <gmloler@gmail.com>
2023-07-31 00:41:08 +0300

    auth admin on users.routes

--- server/routes/users.routes.ts
+++ server/routes/users.routes.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import UserController from "../controllers/users.controller";
 import authMiddleware from "../middlewares/auth.middleware";
+import authAdminMiddleware from "../middlewares/authAdmin.middleware";
 
 const router = express.Router();
 
@@ -11,15 +12,25 @@ router.post("/", UserController.create);
 router.get("/", authMiddleware, UserController.getAll);
 
 // Get a single user by ID
-router.get("/:id", authMiddleware, UserController.getById);
+router.get("/:id", authMiddleware, authAdminMiddleware, UserController.getById);
 
 // Update a user
-router.put("/:id", authMiddleware, UserController.update);
+router.put("/:id", authMiddleware, authAdminMiddleware, UserController.update);
 
 // Delete a user
-router.delete("/:id", authMiddleware, UserController.delete);
+router.delete(
+  "/:id",
+  authMiddleware,
+  authAdminMiddleware,
+  UserController.delete
+);
 
 // Delete all users
-router.delete("/", authMiddleware, UserController.deleteAll);
+router.delete(
+  "/",
+  authMiddleware,
+  authAdminMiddleware,
+  UserController.deleteAll
+);
 
 export default router;
