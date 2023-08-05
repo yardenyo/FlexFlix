0151597b927fc0dba984b8577fd3dc9d8af7a531
yardenyo <gmloler@gmail.com>
2023-08-05 14:52:15 +0300

    check deployment

--- server/routes/app.routes.ts
+++ server/routes/app.routes.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import AppController from "../controllers/app.controller";
 import loggedInMiddleware from "../middlewares/loggedIn.middleware";
+import successResponseMiddleware from "../middlewares/successResponse.middleware";
 
 const router = express.Router();
 
 router.get("/config", loggedInMiddleware, AppController.fetchAppConfig);
 
 router.get("/fetchGeneralSettings", AppController.fetchGeneralSettings);
+
+router.use(successResponseMiddleware);
 
 export default router;
